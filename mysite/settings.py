6950dbdedf8aff6e7d499b2b6b060dd8ba1b1d51
更生汪 <gengswang@hotmail.com>
2024-11-23 17:12:07 +0800

    using mysql

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'watson.mysql.pythonanywhere-services.com',
+        'USER': 'watson',
+        'PASSWORD': 'CloudPrinterDB',
+        'HOST': 'localhost',  # 或者你数据库服务器的地址
+        'PORT': '3306',       # 默认MySQL端口
     }
 }
 
